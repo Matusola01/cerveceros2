d1e526a0921ccc7823438298b8b01754031bb97d
TotiFz <totifernandez10@gmail.com>
2022-11-01 18:43:13 -0300

    responsive text card and lenguage login

--- components/Credential.js
+++ components/Credential.js
@@ -1,22 +1,22 @@
 import Image from 'next/image';
-import { useAuth } from '../context/AuthContext';
 import CredH from '../Images/cred-H.jpeg';
 import CredM from '../Images/cred-M.jpeg';
 
 export default function Credential({ props }) {
-	const { currentUser } = useAuth();
-	console.log(props);
-
-	if (props.genre === 'Hombre') {
+	if (props?.genre === 'Hombre') {
 		return (
 			<div className="flex flex-wrap justify-center mt-20">
 				<div className="relative overflow-hidden flex w-2/3 h-2/3 rounded-r ">
 					<figure className="relative overflow-hidden flex justify-center items-center w-full h-full">
-						<Image src={CredH} alt="Credencial Hombre" className="h-full" />
-						<span className="absolute self-start p-2 text-neutral-900 text-4xl leading-none font-semibold ml-40 mt-16">
-							<h2>{props?.name}</h2>
-							<h2>{props?.DNI}</h2>
-							<h2>{props?.relationship}</h2>
+						<Image src={CredH} className="h-full" />
+						<span className="absolute items-center self-start p-2 text-neutral-900 leading-none font-semibold 2xl:mt-14 xl:mt-10 lg:mt-5 md:mt-4 sm:mt-3">
+							<h2 className="2xl:text-4xl xl:text-3xl lg:text-2xl md:text-xl sm:text-lg">
+								{props?.name}
+							</h2>
+							<h2 className="2xl:text-4xl xl:text-3xl lg:text-2xl md:text-xl sm:text-lg">{props?.DNI}</h2>
+							<h2 className="2xl:text-4xl xl:text-3xl lg:text-2xl md:text-xl sm:text-lg">
+								{props?.relationship}
+							</h2>
 						</span>
 					</figure>
 				</div>
@@ -27,11 +27,15 @@ export default function Credential({ props }) {
 			<div className="flex flex-wrap justify-center mt-20">
 				<div className="relative overflow-hidden flex w-2/3 h-2/3 rounded-r ">
 					<figure className="relative overflow-hidden flex justify-center items-center w-full h-full">
-						<Image src={CredM} alt="Credencial Mujer" className="h-full" />
-						<span className="absolute self-start p-2 text-neutral-900 text-4xl leading-none font-semibold mt-16">
-							<h2>{props?.name}</h2>
-							<h2>{props?.DNI}</h2>
-							<h2>{props?.relationship}</h2>
+						<Image src={CredM} className="h-full" />
+						<span className="absolute items-center self-start p-2 text-neutral-900 leading-none font-semibold 2xl:mt-14 xl:mt-10 lg:mt-5 md:mt-4 sm:mt-3">
+							<h2 className="2xl:text-4xl xl:text-3xl lg:text-2xl md:text-xl sm:text-lg">
+								{props?.name}
+							</h2>
+							<h2 className="2xl:text-4xl xl:text-3xl lg:text-2xl md:text-xl sm:text-lg">{props?.DNI}</h2>
+							<h2 className="2xl:text-4xl xl:text-3xl lg:text-2xl md:text-xl sm:text-lg">
+								{props?.relationship}
+							</h2>
 						</span>
 					</figure>
 				</div>
--- components/Login.js
+++ components/Login.js
@@ -11,14 +11,14 @@ export default function Login() {
 
 	async function submitHandler() {
 		if (!email || !password) {
-			setError('Por favor introduce tu email y contraseña');
+			setError('Please enter email and password');
 			return;
 		}
 		if (isLoggingIn) {
 			try {
 				await login(email, password);
 			} catch (err) {
-				setError('Verifique sus datos e intentelo de nuevo');
+				setError('Incorrect email or password');
 			}
 			return;
 		}
@@ -27,8 +27,8 @@ export default function Login() {
 
 	return (
 		<div className="flex-1 text-xs sm:text-sm flex flex-col justify-center items-center gap-2 sm:gap-4">
-			<h1 className="font-extrabold select-none text-2xl sm:text-4xl uppercase">
-				{isLoggingIn ? 'Login' : 'register'}
+			<h1 className="font-extrabold select-none text-2xl sm:text-4xl uppercase mb-16">
+				{isLoggingIn ? 'Iniciar sesion' : 'Registrarse'}
 			</h1>
 			{error && (
 				<div className="w-full max-w-[40ch] border-rose-400 border text-center border-solid text-rose-400 py-2">
@@ -39,7 +39,7 @@ export default function Login() {
 				type="text"
 				value={email}
 				onChange={e => setEmail(e.target.value)}
-				placeholder="Email"
+				placeholder="Dirección email"
 				className="outline-none duration-300 border-b-2 border-solid border-white focus:border-cyan-300 text-slate-900 p-2 w-full max-w-[40ch]"
 			/>
 			<input
@@ -56,7 +56,7 @@ export default function Login() {
 				<h2 className="relative z-20">Entrar</h2>
 			</button>
 			<h2 className="duration-300 hover:scale-110 cursor-pointer" onClick={() => setIsLoggingIn(!isLoggingIn)}>
-				{!isLoggingIn ? 'Login' : 'Registrate'}
+				{!isLoggingIn ? 'Iniciar sesion' : 'Registrarse'}
 			</h2>
 		</div>
 	);
