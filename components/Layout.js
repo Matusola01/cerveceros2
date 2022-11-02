0dfe3605dd98dcc1b1407f1dede93b0c082a7049
TotiFz <totifernandez10@gmail.com>
2022-11-02 17:49:26 -0300

    CSS Responsive ...

--- components/Layout.js
+++ components/Layout.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default function Layout(props) {
 	const { children } = props;
 	return (
-		<div className="flex flex-col min-h-screen relative bg-slate-900 text-white">
+		<div className="flex flex-col min-h-screen relative bg-slate-300 text-white">
 			<main className="flex-1 flex flex-col p-4">{children}</main>
 		</div>
 	);
