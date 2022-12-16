b36c4827b1a27da6be5805eeceee7903feac6da3
Francisco Fernandez <96742221+TotiFz@users.noreply.github.com>
2022-12-16 17:45:42 -0300

    fixed font css

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -7,7 +7,7 @@ const Hero = ({ heading, message }) => {
 			{/* Overlay */}
 			<div className="absolute top-0 left-0 right-0 bottom-0 bg-black/50 z-[2]" />
 			<div className="p-5 text-white z-[2] mt-[-10rem]">
-				<h2 className="text-5xl font-fb">{heading}</h2>
+				<h2 className="text-6xl font-fb">{heading}</h2>
 				<p className="py-5 text-xl">{message}</p>
 			</div>
 		</div>
