15da5646a659d2cc5fee56dc196be724e988d4e8
Francisco Fernandez <96742221+TotiFz@users.noreply.github.com>
2022-12-16 18:20:40 -0300

    fixed css thinks

--- components/Layout.js
+++ components/Layout.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default function Layout(props) {
 	const { children } = props;
 	return (
-		<div className="flex flex-col min-h-screen relative bg-gray-100 text-white mb-12 custom-img-big md:custom-img-medium sm:custom-img-small min-[320px]:custom-img-phone">
+		<div className="flex flex-col min-h-screen relative bg-gray-100 text-white mb-12 custom-img-big ">
 			<main className="flex-1 flex flex-col p-4 z-[2]">{children}</main>
 		</div>
 	);
